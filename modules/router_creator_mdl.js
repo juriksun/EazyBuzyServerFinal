bf3ebd3c3528202d8684b46a80669969757ee2cf
juriksun <juriksun@gmail.com>
2018-04-04 19:06:32 +0300

    /

--- modules/router_creator_mdl.js
+++ modules/router_creator_mdl.js
@@ -1,7 +1,12 @@
 module.exports = class {
     //no now
-    setTimeWindow(){
+    constructor(){
+        
+    }
 
+    //not for alpha
+    setTimeWindow(){
+    
     }
 
     //
@@ -16,11 +21,62 @@ module.exports = class {
 
     //
     setTaskForSearchPath(allTasksForSearch){
-        this.allTasksForSearch = allTasksForSearch;
+        this.allTasksForSearch = allTasksForSearch
     }
 
+    calcPolygon(){
+        return new Promise((resolve, reject)=>{
 
+        })
+    }
+
+    //not for alpha
+    filterByTime(){
+
+    }
+
+    //
     dispatch(){
+        return new Promise((resolve, reject)=>{
+            this.calcPolygon()
+                .then((polygon) => {
+                this.getSuiteblePlaces(polygon, this.tasks)//alex
+                    .then((suiteblePlaces) => {
+                        this.calcPossibleRoutes(suiteblePlaces)//alex
+                        .then(possibleRoutes => {
+                            this.buildAllRoutesWithSegments(possibleRoutes)//alex
+                            .then(allRoutesWithSegments => {
+                                this.getAllDirectionForRoutesWithSegments(allRoutesWithSegments)//alex
+                                .then(directionsForRoutesWithSegments => {
+                                    this.directionsForRoutesWithSegments = directionsForRoutesWithSegments
+                                    this.chooseRecommendedRoute(directionsForRoutesWithSegments)//alex
+                                    .then(recommendedRoute => resolve(recommendedRoute))
+                                    .catch(errRecommendedRoute => reject({error:"Error"}));
+                                })
+                                .catch(errDirectionsForRoutesWithSegments => reject({error:"Error"}));
+                            })
+                            .catch(errRoutesWithSegments => reject({error:"Error"}));
+                        })
+                        .catch(errPossibleRoutes => reject({error:"Error"}));
+                    })
+                    .catch(errSuiteblePlaces => reject({error:"Error"}));
+                })
+                .catch(errPolygon => reject({error:"Error"}));
+        })
+    };
+
+    calcPolygon() {
+        return new Promise((resolve, reject)=>{
+            resolve([this.start_place.coordinate]);
+        });
         
+    }
+
+    getSuiteblePlaces(){
+
+    }
+
+    saveRoute(){
+
     }
 }
