0d9a03ff2a7c8b1667f2fc376bb7e5c1ae986eb4
unknown <nir@nir-lap.il.webcollage.com>
2018-04-13 18:09:45 +0300

    added task schema

--- models/task_mod.js
+++ models/task_mod.js
@@ -1,15 +1,48 @@
+import { Schema } from 'mongoose';
+
 let mongoose = require('mongoose'),
     schema = mongoose.Schema,
-    userSchema = new schema({
-        first_name: { type : String , required : true },
-        last_name: { type : String , required : true },
-        username: { type : String , required : true , unique : true },
-        email: { type : String , required : true , unique : true },
-        password: { type : String , required : true }
+    shareSchema = new Schema({
+        user_name: { type : String , required : true },
+        share_status: { type : String , required : true }
+    })
+    editTimeSchema = new Schema({
+        create: { type : Number , required : true },
+        last_edited: { type : Number , required : true }
+    })
+    timeSchema = new Schema({
+        start_hour: { type : Number , required : true },
+        end_hour: { type : Number , required : true },
+        duration: { type : Number , required : true }
+    })
+    taskPlaceSchema = new Schema({
+        place_type : { type : String , required : true },
+        palce_key_word : { type : String , required : true }
+    })
+    coordinateSchema = new schema({
+        lat : { type : Number , required : true },
+        long : { type : Number , required : true }
+    }),
+    locationSchema = new schema({
+        address : { type : String , required : true },
+        place_id : { type : String , required : true },
+        coordinate : coordinateSchema
+    }),
+    taskSchema = new schema({
+        id : { type : Number , required : true , unique : true },
+        name : { type : String , required : true },
+        status : { type : String , required : true },
+        priority : { type : Number , required : true },
+        share : shareSchema,
+        edit_time : editTimeSchema,
+        time : timeSchema,
+        task_place : taskPlaceSchema,
+        location : locationSchema
+
     },
     {   strict: true    },
-    {   collection: 'users' });
+    {   collection: 'tasks' });
 
-let Users = mongoose.model('Users',userSchema);
+let Tasks = mongoose.model('Tasks',userSchema);
 
-module.exports = Users;
+module.exports = Tasks;
