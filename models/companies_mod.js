e6f7fe7319ef24e91c833fc5eafdfb59d390013c
juriksun <juriksun@gmail.com>
2018-05-02 05:22:23 +0300

    company_mod.js -> companies_mod.js and change schema structure

--- models/companies_mod.js
+++ models/companies_mod.js
@@ -1,14 +1,18 @@
 const   mongoose = require('mongoose'),
         Schema = mongoose.Schema;
 
-        
 CompanySchema = new Schema({
     formated_company: {type: String},
     company: {type: String}
+});
+
+CompaniesSchema = new Schema({
+    formated_type: {type: String},
+    companys: [CompanySchema]
 },
 {strict: false},
 {collection: 'companies'});
 
-let Company = mongoose.model('Company', CompanySchema);
+let Companiess = mongoose.model('Companies', CompaniesSchema);
 
-module.exports = Company;
+module.exports = Companys;
