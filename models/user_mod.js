57d0e117ed74913e9fed5e0787fa64c9adc1d9e9
NirMekin <nir.mekin@gmail.com>
2018-06-26 23:20:38 +0300

    update user_mod (added image_profile)

--- models/user_mod.js
+++ models/user_mod.js
@@ -5,7 +5,8 @@ let mongoose = require('mongoose'),
         last_name: { type : String , required : true },
         username: { type : String , required : true , unique : true },
         email: { type : String , required : true , unique : true },
-        password: { type : String , required : true }
+        password: { type : String , required : true },
+        image_profile : String
     },
     {   strict: true    },
     {   collection: 'users' });
